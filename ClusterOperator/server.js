510225fce1cc54390ba15d58c68ee98be0529f4a
Ali Mahdavi <alihmahdavi@gmail.com>
2022-06-03 16:33:57 +0300

    fix: getmaster error

--- ClusterOperator/server.js
+++ ClusterOperator/server.js
@@ -4,7 +4,7 @@ const log = require('../lib/log');
 const config = require('./config');
 
 
-Operator.init();
+
 
 const wss = new WebSocketServer({ port: config.apiPort });
 let clients = [];
@@ -12,7 +12,7 @@ let clients = [];
 function handleAPICommand(ws, command, message){
   switch (command) {
     case 'GET_MASTER':
-      ws.send(`{"status":"success","message":"${Operator.getMaster}"}`);
+      ws.send(`{"status":"success","message":"${Operator.getMaster()}"}`);
       break;
     case 'GET_MYIP':
       break;
@@ -35,57 +35,66 @@ function auth(ip){
   if(idx === -1) return true; else return false;
 }
 
-wss.on('connection', function connection(ws, req) {
-  const ip = req.socket.remoteAddress;
-  
-  if(auth(ip)){
-    clients.push({ws:ws, ip:ip});
-    ws.isAlive = true;
-    ws.on('pong', function heartbeat() {
-      this.isAlive = true;
-    });
-    ws.on('message', function message(data) {
-      log.info(`received: ${data}`);
-      try{
-        let jsonData = JSON.parse(data);
-        handleAPICommand(ws, jsonData.command, jsonData.message);
-      }catch(err){
-        log.info('Unrecognized command.');
-      }
-    });
-    ws.on('close', function close() {    
-      let idx = clients.findIndex(item => item.ws==ws);
-      log.info(`socket from ${clients[idx].ip} closed.`);
-      clients = clients.splice(idx,0); 
-    });
-    log.info(`socket connected from ${ip}`);
-    ws.send(`{"status":"connected","from":"${ip}"}`);
-  }else{
-    log.info(`socket connection rejected from ${ip}`);
-    ws.terminate();
-  }
-});
 
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) {
-      let idx = clients.findIndex(item => item.ws==ws);
-      log.info(`connection from ${clients[idx].ip} timed out, terminating socket.`);
-      clients = clients.splice(idx,0); 
-      return ws.terminate();
+
+
+
+
+async function initServer(){
+  //await Operator.init();
+
+  wss.on('connection', function connection(ws, req) {
+    const ip = req.socket.remoteAddress;
+    
+    if(auth(ip)){
+      clients.push({ws:ws, ip:ip});
+      ws.isAlive = true;
+      ws.on('pong', function heartbeat() {
+        this.isAlive = true;
+      });
+      ws.on('message', function message(data) {
+        log.info(`received: ${data}`);
+        try{
+          let jsonData = JSON.parse(data);
+          handleAPICommand(ws, jsonData.command, jsonData.message);
+        }catch(err){
+          log.info('Unrecognized command.');
+        }
+      });
+      ws.on('close', function close() {    
+        let idx = clients.findIndex(item => item.ws==ws);
+        log.info(`socket from ${clients[idx].ip} closed.`);
+        clients = clients.splice(idx,0); 
+      });
+      log.info(`socket connected from ${ip}`);
+      ws.send(`{"status":"connected","from":"${ip}"}`);
+    }else{
+      log.info(`socket connection rejected from ${ip}`);
+      ws.terminate();
     }
-
-    ws.isAlive = false;
-    ws.ping();
   });
-}, 3000);
+  
+  const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+      if (ws.isAlive === false) {
+        let idx = clients.findIndex(item => item.ws==ws);
+        log.info(`connection from ${clients[idx].ip} timed out, terminating socket.`);
+        clients = clients.splice(idx,0); 
+        return ws.terminate();
+      }
+  
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 3000);
+  
+  wss.on('close', function close() {
+    clearInterval(interval);
+  });
+  log.info(`Api Server started on port ${config.apiPort}`);
+  //await Operator.findMaster();
+}
 
-wss.on('close', function close() {
-  clearInterval(interval);
-});
-
-log.info(`Api Server started on port ${config.apiPort}`);
-
-Operator.findMaster();
+initServer();
 
 
