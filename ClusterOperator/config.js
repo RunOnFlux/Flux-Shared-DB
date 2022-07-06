ecebe233ec8dafc3d14859b38933e693e0c78a6c
Ali Mahdavi <alihmahdavi@gmail.com>
2022-07-06 11:43:45 +0300

    bump version

--- ClusterOperator/config.js
+++ ClusterOperator/config.js
@@ -16,7 +16,7 @@ module.exports = {
   containerApiPort: process.env.API_PORT || 33950,
   DBAppName: process.env.DB_APPNAME || 'dbfluxtest4',
   AppName: process.env.CLIENT_APPNAME || '',
-  version: '0.9.49',
+  version: '0.9.50',
   whiteListedIps: process.env.WHITELIST || '::1',
   clusterList: [], // Temporary
 };
