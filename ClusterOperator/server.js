ccca329268ae63960ba183abc289f25182fe3bfd
Ali Mahdavi <alihmahdavi@gmail.com>
2022-09-10 13:31:53 +0300

    fix typo

--- ClusterOperator/server.js
+++ ClusterOperator/server.js
@@ -67,7 +67,7 @@ async function initServer() {
   Security.init();
   startUI();
   await Operator.init();
-  const io = new Server(config.apiPort);
+  const io = new Server();
    const app = new App();
    io.attachApp(app);
   Operator.setServerSocket(io);
