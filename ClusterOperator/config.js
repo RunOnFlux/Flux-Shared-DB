8918e7c4294a30f0e0aec7233efe5a75b7bf3458
Ali Mahdavi <alihmahdavi@gmail.com>
2023-02-08 13:25:01 +0300

    bump version

--- ClusterOperator/config.js
+++ ClusterOperator/config.js
@@ -17,7 +17,7 @@ module.exports = {
   containerApiPort: process.env.API_PORT || 33950,
   DBAppName: process.env.DB_APPNAME || 'wordpressonflux',
   AppName: process.env.CLIENT_APPNAME || '',
-  version: '1.1.9',
+  version: '1.1.10',
   whiteListedIps: process.env.WHITELIST || '::1',
   debugMode: false,
 };
