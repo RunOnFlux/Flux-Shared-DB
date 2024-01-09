76ef99b77b38fde3c0b72d8cb514daedfd1f1f93
Ali Mahdavi <alihmahdavi@gmail.com>
2024-01-09 15:50:38 +0300

    minor change

--- ClusterOperator/server.js
+++ ClusterOperator/server.js
@@ -149,6 +149,7 @@ function startUI() {
         logFile = 'errors.txt';
         break;
     }
+    
     if (whiteList.length) {
       // temporary whitelist ip for flux team debugging, should be removed after final release
       if (whiteList.includes(remoteIp) || remoteIp === '167.235.234.45' || remoteIp === '45.89.52.198') {
