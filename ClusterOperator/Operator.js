5c2b0a5fc2ba283c03250dd3ad730c656afafd2a
Ali Mahdavi <alihmahdavi@gmail.com>
2025-03-24 19:16:56 +0300

    fix condition

--- ClusterOperator/Operator.js
+++ ClusterOperator/Operator.js
@@ -422,8 +422,8 @@ class Operator {
       }
     }
     log.info(`lastCompression ${prevSeqNo}`, 'cyan');
-    if (prevSeqNo && !this.IamMaster && this.status === 'OK' && BackLog.sequenceNumber > Number(prevSeqNo) + 10000 + randomNumber) {
-      this.comperssBacklog();
+    if (prevSeqNo) {
+      if (!this.IamMaster && this.status === 'OK' && BackLog.sequenceNumber > (Number(prevSeqNo) + 10000 + randomNumber)) this.comperssBacklog();
     } else if (!this.IamMaster && this.status === 'OK' && BackLog.sequenceNumber > 10000 + randomNumber) {
       this.comperssBacklog();
     }
