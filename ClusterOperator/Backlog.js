a99c68963d7a56a07dac7bb324f6f83d48fb862b
Ali Mahdavi <alihmahdavi@gmail.com>
2025-05-29 15:53:43 +0300

    disable  restart on error

--- ClusterOperator/Backlog.js
+++ ClusterOperator/Backlog.js
@@ -139,8 +139,8 @@ class BackLog {
           if (false && Array.isArray(BLResult) && BLResult[2]) {
             log.error(`Error in SQL: ${JSON.stringify(BLResult[2])}`);
             if (this.exitOnError) {
-              log.info(`restarting... error executing query, ${query}, ${seq}`, 'red');
-              process.exit(1);
+              log.info(`error executing query, ${query}, ${seq}`, 'red');
+              // process.exit(1);
             }
           } else {
             if (connId === false) {
@@ -153,8 +153,8 @@ class BackLog {
             if (Array.isArray(result) && result[2]) {
               log.error(`Error in SQL: ${JSON.stringify(result[2])}`);
               if (this.exitOnError) {
-                log.info(`restarting... error executing query, ${query}, ${seq}`, 'red');
-                process.exit(1);
+                log.info(`error executing query, ${query}, ${seq}`, 'red');
+                // process.exit(1);
               }
             }
           }
