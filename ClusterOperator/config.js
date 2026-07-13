29e12e19b2fd52eb15eb121d7f1ce41fd00edfb0
Ali Mahdavi <alihmahdavi@gmail.com>
2026-07-13 15:54:27 +0200

    bump version to 1.7.10

--- ClusterOperator/config.js
+++ ClusterOperator/config.js
@@ -17,7 +17,7 @@ module.exports = {
   containerApiPort: String(process.env.API_PORT || 33950).trim(),
   DBAppName: process.env.DB_APPNAME || '',
   AppName: process.env.CLIENT_APPNAME || '',
-  version: '1.7.9',
+  version: '1.7.10',
   whiteListedIps: process.env.WHITELIST || '127.0.0.1',
   debugMode: true,
   containerDataPath: '',
